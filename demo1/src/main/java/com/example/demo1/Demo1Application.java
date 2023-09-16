e7b60f1ab8475c3fd588e2b3980243118aad2f3f
shuT123T <107626697+shuT123T@users.noreply.github.com>
2023-09-16 16:08:19 +0800

    Update Demo1Application.java

--- demo1/src/main/java/com/example/demo1/Demo1Application.java
+++ demo1/src/main/java/com/example/demo1/Demo1Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo1Application {
 
     public static void main(String[] args) {
-        System.out.println("测试git提交")
+        System.out.println("线上")
         SpringApplication.run(Demo1Application.class, args);
     }
 
