88f9287ee72927130f9c72fd3ebcd1c8d37fdffe
TNT <2939798573@qq.com>
2023-09-16 16:33:50 +0800

    分支

--- demo1/src/main/java/com/example/demo1/Demo1Application.java
+++ demo1/src/main/java/com/example/demo1/Demo1Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo1Application {
 
     public static void main(String[] args) {
-        System.out.println("线上")
+        System.out.println("线上");
         SpringApplication.run(Demo1Application.class, args);
     }
 
